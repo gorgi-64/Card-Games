7a75090af9ca23cf412ed615a9c69151889f5e82
gorgi_64 <photographerjorkopeisct@gmail.com>
2026-02-06 13:58:07 +0200

    start na crazy8

---
+++ src/Crazy8.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class Card8 extends Card{
+    @Override
+    public void setScore(int temp){
+        if(temp == 8) score = 80;
+        if(temp >= 11 && temp <= 13) score = 10;
+        if(temp == 14) score = 1;
+        score = temp;
+    }
+    public Card8(int temp){
+        super(temp);
+        setScore(temp);
+    }
+}
+public class Crazy8 {
+    static ArrayList<Card> deck;
+
+    public static void main(String[] args) {
+
+    }
+}
